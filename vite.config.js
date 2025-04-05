57bdedc3386262065976d0a3289c283b37ce7fff
Mahmoud Nagy <nagymedo25@gmail.com>
2025-04-05 20:33:05 +0200

    Update asset paths configuration

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,14 @@ export default defineConfig({
     historyApiFallback: true,
   },
   build: {
-    outDir: 'dist',
+    outDir: 'docs',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
+      },
+    },
   },
 })
